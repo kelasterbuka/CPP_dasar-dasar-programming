730f414e26e4376ad0fe471423a6b84496361df9
cornflake15 <rudyhend@gmail.com>
2021-04-27 14:43:08 +0800

    update binary search

--- 49-Search Array | std lib/main.cpp
+++ 49-Search Array | std lib/main.cpp
@@ -2,41 +2,73 @@
 #include <algorithm>
 #include <array>
 
+using namespace std;
+
+
 const size_t arraySize = 10;
 
-void printArray(std::array <int, arraySize> &angka){
-	std::cout << "Array: ";
+void printarray(array <int, arraySize> &angka){
+	cout << "Array: ";
 	for(int &a : angka){
 		std::cout << a << " ";
 	}
-	std::cout << std::endl;
+	cout << endl;
 }
 
-int main(){
+/* 
+	Fungsi Prototype Binary Search
 
-	std::array <int, arraySize> angka = {9,4,2,5,8,0,1,7,6,3};
-	printArray(angka);
+	Implementasi algoritma Binary Search tanpa menggunakan fungsi standar dari C++.
+	Sumber: https://www.geeksforgeeks.org/binary-search/
+*/
+int binarysearch(int arr[], int l, int r, int x);
 
-	int angkaCari;
+
+
+int main(int argc, char** argv){
+	array <int, arraySize> angka = {9, 4, 2, 5, 8, 0, 1, 7, 6, 3};
+	printarray(angka);
+
+	int cariangka;
 	bool ketemu;
-	// sort dulu
-	// search -> binary_search
-	std::cout << "mencari angka dalam array diatas: \n";
-	std::cin >> angkaCari;
-	std::sort(angka.begin(), angka.end());
-	ketemu = std::binary_search(angka.begin(), angka.end(), angkaCari);
+
+	// Sort dulu
+	// Search -> binary_search
+	cout << "Mencari angka dalam array diatas: \n";
+	cin >> cariangka;
+	sort(angka.begin(), angka.end());
+	ketemu = binary_search(angka.begin(), angka.end(), cariangka);
 
 	if (ketemu){
-		std::cout << "ketemu" << std::endl;	
+		cout << "Ditemukan" << endl;	
 	} else {
-		std::cout << "tidak ketemu" << std::endl;	
+		cout << "Tidak ditemukan" << endl;	
 	}
 	
-	std::cin.get();
 	return 0;
 }
 
+int binarysearch(int arr[], int l, int r, int x){
+	if(r >= l){
+		int mid = l + (r - l) / 2;
 
+		// Jika elemen array yang dicari berada di tengah
+		if(arr[mid] == x){
+			return mid;
+		}
+
+		// Jika elemen array yang dicari berada di sebelah kiri mid
+		if(arr[mid] > x){
+			return binarysearch(arr, l, mid-1, x);
+		}
+
+		// Selain di tengah dan sebelah kiri, maka elemen yg dicari pasti ada di sebelah kanan
+		return binarysearch(arr, mid+1, r, x);
+	}
+
+	// Jika elemen array yang dicari tidak ada di dalam array
+	return -1;
+}
 
 
 
